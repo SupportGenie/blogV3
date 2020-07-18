fb279f7446b0601a6151526ffffcdfec7ac5d9f5
tarun <agumbe22@gmail.com>
2020-07-18 15:16:01 +0530

    Correct google analytics tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "UA-164743872-1",
+        // supportgenie tracking code
+        trackingId: "UA-72864533-1",
         head: true,
       }
     },
